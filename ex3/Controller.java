062f9fe8d2e10495b620413a908d0057e6038909
Noah <noah.buchs@outlook.com>
2021-01-20 11:13:20 +0100

    ex3 done

---
+++ ex3/Controller.java
@@ -0,0 +1,70 @@
+package ex3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+
+public class Controller {
+    int moon = 384403;
+
+    
+    @FXML
+    public void handleCloseButtonAction(ActionEvent event) {
+        ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
+    }
+    @FXML
+    private void checkDays() {
+        days.setSelected(days.isSelected());
+        hours.setSelected(false);
+    }
+
+    @FXML
+    private void checkHours() {
+        hours.setSelected(hours.isSelected());
+        days.setSelected(false);
+    }
+    @FXML
+    private TextField outputtravel;
+
+
+    @FXML
+    private TextField traveltime;
+    @FXML
+    private RadioButton hours;
+    @FXML
+    private RadioButton days;
+
+
+
+    @FXML
+
+    private void calculate() {
+        try {
+            int speed = Integer.parseInt(traveltime.getText());
+
+            if(hours.isSelected()){
+                days.setSelected(false);
+                int result = moon / speed;
+                outputtravel.setText(String.valueOf(result));
+            }
+            else if(days.isSelected()){
+                hours.setSelected(false);
+                int result = moon / speed / 24;
+                outputtravel.setText(String.valueOf(result));
+            }
+        } catch (Exception e) {
+            Error();
+        }
+    }
+    @FXML
+    private void Error() {
+        traveltime.setPromptText("Field required");
+
+    }
+
+
+}
+
+
---
+++ ex3/Main.java
@@ -0,0 +1,25 @@
+package ex3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ex3.fxml"));
+        primaryStage.setTitle("Exercise 3");
+        primaryStage.setScene(new Scene(root, 500, 250));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
